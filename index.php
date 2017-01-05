0639b2ab82038af623186f815d0e5dd68be74a06
Tiago Murakami <trmurakami@gmail.com>
2017-01-05 16:21:00 -0200

    Busca por tags

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         
         <div class="uk-grid uk-margin-large-bottom" data-uk-grid-margin>
             <div class="uk-width-medium-1-1">
-                <div class="uk-vertical-align uk-text-center uk-responsive-width" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 650px;">
+                <div class="uk-vertical-align uk-text-center uk-responsive-width" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 750px;">
                     <div class="uk-vertical-align-middle uk-width-1-2">
                         <br/>
                         <h1>Coleta Produção USP</h1>
@@ -47,6 +47,15 @@
                                 <input type="hidden" name="fields[]" value="titulo">
                                 <input type="hidden" name="fields[]" value="autores">
                                 <button class="uk-button-primary">Buscar</button><br/>                                    
+                            </fieldset>
+                        </form>
+                        <br/>
+                        <form class="uk-form" action="result_trabalhos.php" method="get">
+                            <fieldset data-uk-margin>
+                                <legend>Pesquisa por TAG</legend>
+                                <input type="text" placeholder="Pesquise por tag" class="uk-form-width-large" name="search[]" data-validation="required">                                        
+                                <input type="hidden" name="fields[]" value="tag">
+                                <button class="uk-button-primary">Buscar tag</button><br/>                                    
                             </fieldset>
                         </form>
                         <br/>
@@ -64,7 +73,7 @@
                             <fieldset data-uk-margin>
                                 <legend>Inserir ID do curriculo que deseja incluir</legend>
                                 <input type="text" placeholder="Insira o ID do Curriculo" class="uk-form-width-medium" name="id_lattes" data-validation="required">
-                                <input type="text" placeholder="Tag para formar um grupo" class="uk-form-width-medium" name="tag">
+                                <input type="text" placeholder="TAG para formar um grupo" class="uk-form-width-medium" name="tag">
                                 <button class="uk-button-primary">Incluir</button><br/>                                    
                             </fieldset>
                         </form>
@@ -73,7 +82,7 @@
                             <fieldset data-uk-margin>
                                 <legend>Inserir um DOI de artigo que queira incluir (sem http://dx.doi.org/)</legend>
                                 <input type="text" placeholder="Insira um DOI" class="uk-form-width-medium" name="doi" data-validation="required">
-                                <input type="text" placeholder="Tag para formar um grupo" class="uk-form-width-medium" name="tag">
+                                <input type="text" placeholder="TAG para formar um grupo" class="uk-form-width-medium" name="tag">
                                 <button class="uk-button-primary">Incluir</button><br/>                                    
                             </fieldset>
                         </form>
