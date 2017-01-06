c66370e8784264181a663e14aa586f3576569833
Tiago Murakami <trmurakami@gmail.com>
2017-01-06 10:11:48 -0200

    Parser do Scopus

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         
         <div class="uk-grid uk-margin-large-bottom" data-uk-grid-margin>
             <div class="uk-width-medium-1-1">
-                <div class="uk-vertical-align uk-text-center uk-responsive-width" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 750px;">
+                <div class="uk-vertical-align uk-text-center uk-responsive-width" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 850px;">
                     <div class="uk-vertical-align-middle uk-width-1-2">
                         <br/>
                         <h1>Coleta Produção USP</h1>
@@ -71,7 +71,7 @@
                         <br/>
                         <form class="uk-form" action="jsontoelastic.php" method="get">
                             <fieldset data-uk-margin>
-                                <legend>Inserir ID do curriculo que deseja incluir</legend>
+                                <legend>Inserir ID do Currículo Lattes que deseja incluir</legend>
                                 <input type="text" placeholder="Insira o ID do Curriculo" class="uk-form-width-medium" name="id_lattes" data-validation="required">
                                 <input type="text" placeholder="TAG para formar um grupo" class="uk-form-width-medium" name="tag">
                                 <button class="uk-button-primary">Incluir</button><br/>                                    
@@ -96,6 +96,15 @@
                             </fieldset>
                         </form>                         
                         <br/>
+                        <form class="uk-form" action="scopus_upload.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+                            <fieldset data-uk-margin>
+                                <legend>Enviar um arquivo do Scopus (CSV - All available information)</legend>
+                                <input type="file" name="file">
+                                <input type="text" placeholder="Tag para formar um grupo" class="uk-form-width-medium" name="tag">
+                                <button class="uk-button-primary" name="btn_submit">Upload</button><br/>                                    
+                            </fieldset>
+                        </form>                         
+                        <br/>                        
                     </div>
                 </div>
             </div>
---
+++ scopus_upload.php
@@ -0,0 +1,99 @@
+<?php
+
+    include('inc/config.php');             
+    include('inc/functions.php');
+
+    if (isset($_FILES['file'])) {    
+        $fh = fopen($_FILES['file']['tmp_name'], 'r+');
+        while( ($row = fgetcsv($fh, 8192,",")) !== FALSE ) {    
+            
+            //print_r($row);
+            //echo '<br/><br/>'; 
+            
+            // Palavras chave
+            $palavras_chave_authors = explode(";",$row[16]);
+            $palavras_chave_wos = explode(";",$row[17]);
+            $palavras_chave_array = array_merge($palavras_chave_authors, $palavras_chave_wos);
+            
+            // Autores
+            $autores_nome_array = explode(",",$row[0]);
+            $autores_afiliacao_array = explode(";",$row[14]);
+            $autores_json_str = [];
+            
+            for($i=0;$i<count($autores_nome_array);$i++){
+                $autores_json_str[] = '{"nome_completo_do_autor":"'.$autores_nome_array[$i].'","nome_e_afiliacao":"'.$autores_afiliacao_array[$i].'"}';
+            }
+            
+            // Agência de fomento
+            $agencia_de_fomento_array = explode(";",$row[26]);
+            
+            // Afiliação
+            $afiliacao_array = explode(",",$row[13]);            
+
+            if ($row[38] == "Article") {
+                $container_title = '
+					"periodico":{
+						"titulo_do_periodico":"'.str_replace('"','',$row[3]).'",
+                        "nome_da_editora":"'.$row[25].'",
+						"issn":"'.$row[32].'",
+						"volume":"'.$row[4].'",
+						"fasciculo":"'.$row[5].'",
+						"pagina_inicial":"'.$row[7].'",
+						"pagina_final":"'.$row[8].'"
+					},
+                ';
+            }
+            
+            if (!empty($row[11])) {
+                $sha256 = hash('sha256', ''.$row[11].'');
+            } else {
+                $sha256 = hash('sha256', ''.$row[40].'');
+            }
+            
+            $query_scopus = 
+                '{
+                    "doc":{
+                        "source":"Base Scopus",  
+                        "scopus_id": "'.$row[40].'",
+                        "tag": ["'.$_POST["tag"].'"],
+                        "tipo":"'.$row[38].'",
+                        "titulo": "'.str_replace('"','',$row[1]).'",
+                        "ano": "'.$row[2].'",
+                        "idioma": "'.$row[36].'",
+                        "doi":"'.$row[11].'",
+                        "citacoes_recebidas": "'.$row[10].'",
+                        "afiliacao":["'.implode('","',str_replace('"','',$afiliacao_array)).'"],
+                        "agencia_de_fomento":["'.implode('","',str_replace('"','',$agencia_de_fomento_array)).'"],
+                        '.$container_title.'
+                        "resumo":"'.str_replace('"','',$row[15]).'",
+                        "palavras_chave":["'.implode('","',$palavras_chave_array).'"],	
+                        "autores":['.implode(',',$autores_json_str).']
+
+                    },
+                    "doc_as_upsert" : true
+                }';
+//            
+//            print_r($query_scopus);
+//            echo '<br/><br/>';
+//            
+            $resultado = store_record($client,$sha256,$query_scopus);
+            print_r($resultado);  
+            
+            
+            //Limpar variáveis
+            unset($palavras_chave_authors);
+            unset($palavras_chave_wos);
+            unset($palavras_chave_array);
+            unset($autores_array);
+            unset($autores_json_str);
+            
+        }
+    }
+    
+    sleep(5); 
+    echo '<script>window.location = \'http://bdpife2.sibi.usp.br/coletaprod/result_trabalhos.php?search[]=tag.keyword:"'.$_POST["tag"].'"\'</script>';
+
+?>
+
+    
+    
