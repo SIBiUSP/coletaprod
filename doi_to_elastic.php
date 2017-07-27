b007c2a07ff80a32638ae359b3e0127dfbb7b5ec
Tiago Murakami <trmurakami@gmail.com>
2017-07-27 15:58:57 -0300

    Correção do endereço final

--- doi_to_elastic.php
+++ doi_to_elastic.php
@@ -6,7 +6,7 @@ include('inc/functions.php');
 if (isset($_GET["doi"])) {
     dadosExternos::query_doi($_GET["doi"],$_GET["tag"]);
     sleep(5); 
-    echo '<script>window.location = \'http://bdpife2.sibi.usp.br/dev_coletaprod/result_trabalhos.php?search[]=doi.keyword:"'.$_GET["doi"].'"\'</script>';
+    echo '<script>window.location = \'http://coletaprod.sibi.usp.br/result_trabalhos.php?search[]=doi.keyword:"'.$_GET["doi"].'"\'</script>';
 } else {
     echo '<p>Favor inserir um DOI</p>';
 }
