d6c289df1f18fcd9ce1dfdc6d0e5b2cd303ba671
Tiago Murakami <trmurakami@gmail.com>
2017-08-28 16:53:45 -0300

    Jquery Local

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -7,15 +7,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="inc/images/faviconUSP.ico" type="image/x-icon">
 
-<!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.25/css/uikit.min.css" />
 
-<!-- jQuery is required -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Jquery -->
+<script src="inc/jquery/jquery.min.js"></script>
+<script src="inc/jquery/jquery-ui.js"></script>
+<link rel="stylesheet" href="inc/jquery/jquery-ui.css">
+<script src="inc/jquery/jquery.form-validator.min.js"></script> 
 
-<!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.25/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.25/js/uikit-icons.min.js"></script>
+<!-- Uikit - Local -->
+<link rel="stylesheet" href="inc/uikit/css/uikit.min.css" />
+<script src="inc/uikit/js/uikit.min.js"></script>
 
 <!-- Validador -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script> 
