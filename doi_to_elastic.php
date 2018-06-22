ee3da2522e2f0a604c7660339d84bc7146662f32
Tiago Murakami <trmurakami@bdpife2.sibi.usp.br>
2018-06-22 13:45:12 -0300

    Correção do retorno

--- doi_to_elastic.php
+++ doi_to_elastic.php
@@ -6,7 +6,7 @@ include('inc/functions.php');
 if (isset($_GET["doi"])) {
     DadosExternos::query_doi($_GET["doi"], $_GET["tag"]);
     sleep(5); 
-    echo '<script>window.location = \'http://coletaprod.sibi.usp.br/result_trabalhos.php?search[]=doi.keyword:"'.$_GET["doi"].'"\'</script>';
+    echo '<script>window.location = \'http://coletaprod.sibi.usp.br/coletaprod/result_trabalhos.php?search[]=doi.keyword:"'.$_GET["doi"].'"\'</script>';
 } else {
     echo '<p>Favor inserir um DOI</p>';
 }
