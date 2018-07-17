f7ea651cdab0311692f1f74edc496d27d4bb700a
Tiago Murakami <trmurakami@bdpife2.sibi.usp.br>
2018-07-17 14:04:39 -0300

    Correção

--- scopus_upload.php
+++ scopus_upload.php
@@ -97,7 +97,7 @@ if (isset($_FILES['file'])) {
 }
 
 sleep(5);
-echo '<script>window.location = \'result_trabalhos.php?search[]=tag.keyword:"'.$_POST["tag"].'"\'</script>';
+echo '<script>window.location = \'result_trabalhos.php?filter[]=type:"Work"&filter[]=tag:"'.$_POST["tag"].'"\'</script>';
 
 class Record
 {
